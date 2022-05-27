2e0e2f9186242f0f9e7a4a5e186f06107c788bb9
macaferrari <101413589+macaferrari@users.noreply.github.com>
2022-05-27 03:06:40 +0000

    listo la lectura del archivo

--- .ccls-cache/@home@runner@FERRARI-MACARENA-Tarea3/main.cpp
+++ .ccls-cache/@home@runner@FERRARI-MACARENA-Tarea3/main.cpp
@@ -17,23 +17,20 @@ int main(int argc, char ** argv) {
   int cont_peleador = 0;
   int cont_obj = 0;
   int cont_arena = 0;
+  
   while(getline(entrada, linea)) {
     stringstream stream(linea);
-    string type, t,  name, tipo, salud, fuerza, vel, intel, resist, leyenda;
+    string type,  name, tipo, salud, fuerza, vel, intel, resist, leyenda;
     getline(stream, type, delimitador);
     type.erase(0,1);
-    cout<<type<<endl;
-    
 
-    
-    
+  //si es peleador
     if (type == "Peleador" || type == "peleador") {
       cont_peleador++;
-      cout<<"es un peleador\n";
+      
       getline(stream, name, delimitador);
       getline(stream, salud, delimitador);
       int sal = stoi(salud);
-      cout<<salud<<endl;
       getline(stream, fuerza, delimitador);
       int fuer = stoi(fuerza);
       getline(stream, vel, delimitador);
@@ -43,18 +40,68 @@ int main(int argc, char ** argv) {
       getline(stream, resist, delimitador);
       int res = stoi(resist);
       getline(stream, leyenda, delimitador);
+      int len_ley = leyenda.length();
+      leyenda.erase(0, 2);
+      leyenda.erase((len_ley - 6), 6);
 
-      switch(cont_peleador) {
-        case 0:
-          peleador peleador1(name, sal, fuer, velo, inte, res, leyenda);
-          break;
 
-        case 1:
-          peleador peleador2(name, sal, fuer, velo, inte, res, leyenda);
-          break;
+      if (cont_peleador == 1) {
+        peleador peleador1(name, sal, fuer, velo, inte, res, leyenda);
+      }
+      else if (cont_peleador == 2) {
+        peleador peleador2(name, sal, fuer, velo, inte, res, leyenda);
+      }
+        
+      
+       
+    }
+    //para los objetos
+    else if (type == "objeto" || type == "Objeto") {
+      cont_obj++;
+      getline(stream, name, delimitador);
+      getline(stream, tipo, delimitador);
+      getline(stream, salud, delimitador);
+      getline(stream, fuerza, delimitador);
+      getline(stream, vel, delimitador);
+      getline(stream, intel, delimitador);
+      getline(stream, resist, delimitador);
+      getline(stream, leyenda, delimitador);
+      int len_ley = leyenda.length();
+      leyenda.erase(0, 2);
+      leyenda.erase((len_ley - 6), 6);
+
+      
+
+      if (cont_obj == 1) {
+        objeto objeto1(name, tipo, salud, fuerza, vel, intel, resist, leyenda);
+      }
+      else if (cont_obj == 2) {
+        objeto objeto2(name, tipo, salud, fuerza, vel, intel, resist, leyenda);
       }
       
-      
+
+    }
+
+    //para la arena
+    else if (type == "Arena" || type == "arena") {
+      cont_arena++;
+      getline(stream, name, delimitador);
+      getline(stream, salud, delimitador);
+      getline(stream, fuerza, delimitador);
+      getline(stream, vel, delimitador);
+      getline(stream, intel, delimitador);
+      getline(stream, resist, delimitador);
+      getline(stream, leyenda, delimitador);
+      int len_ley = leyenda.length();
+      leyenda.erase(0, 2);
+      leyenda.erase((len_ley - 6), 6);
+
+
+      arena arena(name, salud, fuerza, vel, intel, resist, leyenda);
+
+      if (cont_arena != 1) {
+        cout<<"Ya existe una arena para esta batalla"<<endl;
+      }
     }
   }
   
